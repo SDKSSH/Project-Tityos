d8a9f0e0ce57e75f71a31e7a5f6c602780ff437e
SDKSSH <91608137+SDKSSH@users.noreply.github.com>
2022-12-07 11:53:26 +0100

    Create doigdrip.py

---
+++ easteregg/doigdrip.py
@@ -0,0 +1,21 @@
+doigdrip = ""
+
+def lireFichier():
+    '''
+    Lis le fichier ascii du doigdrip et le met dans la variable global :D
+    '''
+    global doigdrip
+    f = open("./doigdrip.txt", "r", encoding="utf-8")
+    l = f.readline()
+    while(l != ""):
+        doigdrip = doigdrip+l
+        l = f.readline()
+    f.close()
+
+
+def AfficherDoigby():
+    '''
+    Affiche le dieu, le tous puissant, DOIGBY !!!!!!
+    '''
+    global doigdrip
+    print(doigdrip)
