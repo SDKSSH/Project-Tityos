2fdfc9d2d58b7063f7c5c90d00b651c5a1bd0cfe
SDKSSH <91608137+SDKSSH@users.noreply.github.com>
2022-12-14 12:00:49 +0100

    Create Items.py

---
+++ items/Items.py
@@ -0,0 +1,16 @@
+epee_briser = {
+    "name" : "Épée Briser",
+    "damage" : 1,
+    "sell" : 0
+}
+sceptre_defaillant = {
+    "name" : "Sceptre Défaillant",
+    "damage" : 1,
+    "sell" : 0
+}
+lance_pierre = {
+    "name" : "Lance Pierre En Bois De Sapin",
+    "damage" : 1,
+    "sell" : 0
+}
+
