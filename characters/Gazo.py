dd3d30f95ab4bcb808040c2b3abb4e1cd8ec6232
SDKSSH <91608137+SDKSSH@users.noreply.github.com>
2022-12-31 15:29:21 +0100

    Adding gazo theme and gameover

--- characters/Gazo.py
+++ characters/Gazo.py
@@ -1,17 +1,39 @@
 from random import randint
 from i18n.Texts import getText
+import multiprocessing
+from playsound import playsound
+import models.Player as Player
 
 
 damage = 1
 attacks = ["gazo_at1", "gazo_at2", "gazo_at3", "gazo_at4"]
 health = 4
+inFight = False
+sound_thread = None
 
-def attack():
+def playGazoTheme():
+    global sound_thread
+    sound_thread = multiprocessing.Process(target=playsound, args=("file.mp3",))
+    sound_thread.start()
+    
+def stopGazoTheme():
+    global sound_thread
+    sound_thread.close()
+    sound_thread = None
+
+def attack() -> bool:
     '''
-    Gazo attaque mdr
+    Gazo attaque
+    Il retourne a la fin si le fight continue ou est perdu
     '''
     if(randint(0,100) >= 90):
         print(getText("attack_tmp") % ("Gazo", getText("gazo_at4"), 667))
-        #Win
+        stopGazoTheme()
+        return False
     else:
         print(getText("attack_tmp") % ("Gazo", getText(attacks[randint(0, 3)]), damage))
+        Player.health = Player.health-damage
+        if Player.health <= 0:
+            return False
+        else:
+            return True
