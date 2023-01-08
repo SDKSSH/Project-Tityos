b68417482a53750c5fca00b066df7f9d07324569
SDKSSH <91608137+SDKSSH@users.noreply.github.com>
2023-01-08 22:26:28 +0100

    Finish Marchand

--- items/Items.py
+++ items/Items.py
@@ -4,82 +4,94 @@ epee_briser = {
     "id" : 0,
     "name" : "Épée Briser",
     "damage" : 1,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 0
 }
 sceptre_defaillant = {
     "id" : 1,
     "name" : "Sceptre Défaillant",
     "damage" : 1,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 0
 }
 lance_pierre = {
     "id" : 2,
     "name" : "Lance Pierre En Bois De Sapin",
     "damage" : 1,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 0
 }
 
 sceptre_brillant = {
     "id" : 3,
     "name" : "Sceptre Brillant",
     "damage" : 2,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 10
 }
 
 epee_en_bois = {
     "id" : 4,
     "name" : "Épée En Bois",
     "damage" : 2,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 10
 }
 
 epee_en_metal = {
     "id" : 5,
     "name" : "Épée En Métal",
     "damage" : 4,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 20
 }
 
 sceptre_metalique = {
     "id" : 6,
     "name" : "Sceptre Métallique",
     "damage" : 5,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 20
 }
 
 epee_lunatique = {
     "id" : 7,
     "name" : "Épée Lunatique",
     "damage" : 8,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 40
 }
 
 sceptre_de_lumiere = {
     "id" : 8,
     "name" : "Sceptre De Lumière",
     "damage" : 10,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 40
 }
 
 arc_neuf = {
     "id" : 9,
     "name" : "Arc Neuf",
     "damage" : 2,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 10
 }
 
 arc_en_bois = {
     "id" : 10,
     "name" : "Arc En Bois",
     "damage" : 4,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 20
 }
 
 arc_sacre = {
     "id" : 11,
     "name" : "Arc Sacré",
     "damage" : 10,
-    "sell" : 0
+    "sell" : 0,
+    "price" : 40
 }
 
 def getItemByID(id : int) -> Optional[dict]:
--- marchand/Marchand.py
+++ marchand/Marchand.py
@@ -4,7 +4,7 @@ from pick import pick
 import easteregg.doigdrip as doigdrip
 import models.Player as Player
 import items.Items as Items
-def spawnMarchand():
+def spawnMarchand(cb):
     '''
     Fait apparaître le marchand qui est doigby
     '''
@@ -21,4 +21,26 @@ def spawnMarchand():
     elif(Player.getClasse()["name"] == "Archer"):
         opt = [9,10, 11]
     options, selected = pick([Items.getItemByID(x) for x in opt], "Que voulez-vous acheter ?", indicator="=>")
-    
+    it = Items.getItemByName(options)
+    sendPick(opt, cb)
+
+def sendPick(opt, cb):
+    '''
+    Envoie un pick avec les options
+    '''
+    optt = [Items.getItemByID(x) for x in opt]
+    optt.append("Quitter")
+    options, selected = pick(optt, "Que voulez-vous acheter ?", indicator="=>")
+    if(options == "Quitter"):
+        cb()
+    else:
+        it = Items.getItemByName(options)
+        if(Player.getMoney() >= it["price"]):
+            Player.removeCoin(it["price"])
+            Player.setWeapon(it)
+            print("Vous avez acheté %s" % (it["name"]))
+            sleep(1)
+        else:
+            print("Vous n'avez pas assez d'argent !")
+            sleep(1)
+            sendPick(opt)
